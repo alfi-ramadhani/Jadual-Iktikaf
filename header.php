96b2f7a79936d052b5395cc81ef5c71861c0962c
Alfi Ramadhani <info.alfistudio@gmail.com>
2019-05-26 23:41:44 +0800

    First Commit

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="robots" content="noindex">
+  <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css" >
+  
+  <link rel="shortcut icon" href="images/favicon.png" />
+  <link rel="image_src" href="images/iktikaf-isma-shah-alam.jpg" />  
+  <meta name="description" content="Iktikaf Perdana | ISMA Shah Alam"/>
+  <meta property="og:locale" content="ms_my" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Iktikaf Perdana | ISMA Shah Alam" />
+  <meta property="og:description" content="Sistem pengolahan surat jualan automatik" />
+  <meta property="og:site_name" content="Iktikaf Perdana | ISMA Shah Alam" />
+  <meta property="og:image" content="images/iktikaf-isma-shah-alam.jpg" />
---
+++ index.php
@@ -0,0 +1,161 @@
+<?php include 'header.php';?>
+  <title>Iktikaf Perdana | ISMA Shah Alam</title>
+</head>
+<body>
+
+<!--
+Keterangan:		Program Iktikaf Perdana ISMA Shah Alam 2019
+Pemilik:		ISMA Shah Alam
+Web Pemilik:	https://www.ismashahalam.net
+Pengaturcara:	ALFI Studio
+Author URI:		https://studio.alfiya.com.my/
+Versi:			1.0
+CSS:			Bootstrap 4.1
+-->
+
+<div class="container">
+
+<div id="carouzel" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/header.svg" alt="header">
+    </div>
+  </div>
+</div>
+
+<h2 class="page-header m-3">Tentatif Iktikaf Perdana</h2>
+
+<ul class="nav nav-tabs" role="tablist">
+  <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#home" role="tab"><b>Jum, 24 Mei</b></a></li>
+  <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#menu1" role="tab"><b>Sab, 25 Mei</b></a></li>
+  <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#menu2" role="tab"><b>Aha, 26 Mei</b></a></li>
+</ul>
+
+<div class="tab-content">
+  <div id="home" class="tab-pane fade show active" role="tabpanel">
+    <table class="table table-hover mt-3">
+      <thead>
+        <tr class="table-primary">
+          <th scope="row">Waktu</th>
+          <td>Aktiviti</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="table-light">
+          <th scope="row">9 PM</th>
+          <td>Solat Isyak & tarawih</td>
+        </tr>
+        <tr>
+          <th scope="row">10 PM</th>
+          <td>Taklimat program</td>
+        </tr>
+        <tr class="table-light">
+          <th scope="row">10.30 PM</th>
+          <td>Tadarus</td>
+        </tr>
+      </tbody>
+    </table> 
+  </div>
+  <div id="menu1" class="tab-pane fade" role="tabpanel">
+    <table class="table table-hover mt-3">
+      <thead>
+        <tr class="table-primary">
+          <th scope="row">Waktu</th>
+          <td>Aktiviti</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="table-light">
+          <th scope="row">4.30 AM</th>
+          <td>Qiamullail</td>
+        </tr>
+        <tr>
+          <th scope="row">5 AM</th>
+          <td>Sahur, solat Subuh</td>
+        </tr>
+        <tr class="table-light">
+          <th scope="row">7 AM</th>
+          <td>Khidmat diri</td>
+        </tr>
+        <tr>
+          <th scope="row">9 AM</th>
+          <td>Kelas tajwid</td>
+        </tr>
+        <tr class="table-light">
+          <th scope="row">10.30 AM</th>
+          <td>Kuiz</td>
+        </tr>
+        <tr>
+          <th scope="row">12 PM</th>
+          <td>Bebas</td>
+        </tr>
+        <tr class="table-light">
+          <th scope="row">1 PM</th>
+          <td>Solat Zuhur</td>
+        </tr>
+        <tr>
+          <th scope="row">2.30 PM</th>
+          <td>Taklimat rehlah Masyi</td>
+        </tr>
+        <tr class="table-light">
+          <th scope="row">3 PM</th>
+          <td>Rehlah Masyi</td>
+        </tr>
+        <tr>
+          <th scope="row">4.30 PM</th>
+          <td>Solat Asar</td>
+        </tr>
+        <tr class="table-light">
+          <th scope="row">6 PM</th>
+          <td>Khidmat diri, berbuka, solat Maghrib, Isyak & tarawih</td>
+        </tr>
+        <tr>
+          <th scope="row">10.30 PM</th>
+          <td>Tadarus</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div id="menu2" class="tab-pane fade" role="tabpanel">
+    <table class="table table-hover mt-3">
+      <thead>
+        <tr class="table-primary">
+          <th scope="row">Waktu</th>
+          <td>Aktiviti</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="table-light">
+          <th scope="row">4.30 AM</th>
+          <td>Qiamullail</td>
+        </tr>
+        <tr>
+          <th scope="row">5 AM</th>
+          <td>Sahur, solat Subuh</td>
+        </tr>
+        <tr class="table-light">
+          <th scope="row">7 AM</th>
+          <td>Khidmat diri</td>
+        </tr>
+        <tr>
+          <th scope="row">8.30 AM</th>
+          <td>Kelas tajwid</td>
+        </tr>
+        <tr class="table-light">
+          <th scope="row">10 AM</th>
+          <td>Ceramah Uighur</td>
+        </tr>
+        <tr>
+          <th scope="row">11.30 AM</th>
+          <td>Penutup</td>
+        </tr>
+        <tr class="table-light">
+          <th scope="row">12 PM</th>
+          <td>Bersurai</td>
+        </tr>
+      </tbody>
+    </table> 
+  </div>
+</div>
+
+<?php include 'footer.php' ?>
